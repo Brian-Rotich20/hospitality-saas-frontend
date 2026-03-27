3054f697a5952f456e544432afcc609fe6b8fe69
Brian-Rotich20 <ralphrotich@gmail.com>
2026-03-27 23:54:57 +0300

    fix the upload image

--- next.config.ts
+++ next.config.ts
@@ -16,7 +16,7 @@ const nextConfig = {
     return [
       {
         source:      '/api/:path*',
-        destination: `${process.env.BACKEND_URL}/api/:path*`,
+        destination: `${process.env.BACKEND_URL}/:path*`,
       },
     ];
   },
